f2d90a4109c9452c352259d7c3924318cc402aa4
xusheng <klein203@hotmail.com>
2017-06-21 18:35:37 +0800

    20170621

--- Pydev/src/com/pydev/course/Test.py
+++ Pydev/src/com/pydev/course/Test.py
@@ -4,6 +4,45 @@ Created on 2017��6��21��
 @author: xusheng
 '''
 
+import logging
+import pickle
+
+
+
+def catchexpection():
+    def foo(s):
+        return 10 / int(s)
+
+    def bar(s):
+        return foo(s) * 2
+    
+    try:
+        bar('0')
+    except Exception as e:
+        logging.exception(e)
+    finally:
+        print('finally...')
+
+def withfile():
+    with open('d:\\tmp\\1-1.jpg', 'br') as fi:
+        buf = fi.read()
+        with open('d:\\tmp\\1-1-backup.jpg', 'bw') as fo:
+            fo.write(buf)
+
+def dumpfile():
+    with open('d:\\tmp\\1-1.jpg', 'br') as fi:
+        buf = fi.read()
+        with open('d:\\tmp\\1-1.dump', 'bw') as fo:
+            pickle.dump(buf, fo)
+
+def restorefile():
+    with open('d:\\tmp\\1-1.dump', 'br') as fi:
+        d = pickle.load(fi)
+        with open('d:\\tmp\\1-1-restore.jpg', 'bw') as fo:
+            fo.write(d)
+    
+
 if __name__ == '__main__':
-    print('hello world!')
-    pass
+    dumpfile()
+    restorefile()
+        
